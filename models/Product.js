2e993d307006b434fa03e08db00201d779c7f3c1
Terrence Chan <terrencechan.writer@gmail.com>
2021-03-06 16:49:11 -0500

    Add columns to models

--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,9 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
+const { now } = require('sequelize/types/lib/utils');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -9,7 +11,28 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER
+    }
   },
   {
     sequelize,
