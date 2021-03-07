9dd01ee962b3fd325ecb35872d8f8d0f8009559b
Terrence Chan <terrencechan.writer@gmail.com>
2021-03-07 13:57:18 -0500

    Add foreignKey properties models/index.js.

--- models/index.js
+++ models/index.js
@@ -5,16 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+  foreignKey: product_id
+});
 
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product, {
+  foreignKey: category_id
+});
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(ProductTag);
+Product.belongsToMany(ProductTag, {
+  foreignKey: product_id
+});
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(ProductTag);
+Tag.belongsToMany(ProductTag, {
+  foreignKey: tag_id
+});
 
 module.exports = {
   Product,
